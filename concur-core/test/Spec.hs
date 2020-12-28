8b5dfc59735b140254c25b932a59b0f29f5314a0
kamoii <>
2020-12-29 06:13:26 +0900

    Add more test

--- concur-core/test/Spec.hs
+++ concur-core/test/Spec.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE TypeApplications #-}
 import Concur.Core
 import Control.Monad.Free (Free (Free, Pure))
 import Data.Void (Void)
 import Test.Tasty
 import Test.Tasty.HUnit (testCase, (@?=))
 import Control.Concurrent (threadDelay)
+import Control.Applicative (Alternative((<|>)))
 
 data WidgetOp v a
     = WOView v
@@ -28,13 +30,24 @@ main =
             "All"
             [ testDisplay
             , testIO
+            , testAlternative
             ]
 
+testAlternative = testCase "Alternative" $ do
+    let io = liftSafeBlockingIO (threadDelay 10)
+    ops0 <- runWidget $ io <|> display "a"
+    ops0 @?= [WOView "a", WODone ()]
+
+    ops1 <- runWidget $ do
+        io <|> display "a"
+        display @_ @Void "b"
+    ops1 @?= [WOView "a", WOView "b", WOForever]
+
 testIO = testCase "io" $ do
-    ops <- runWidget $ liftSafeBlockingIO (threadDelay 10)
-    ops @?= [WODone ()]
-    ops <- runWidget $ liftUnsafeBlockingIO (threadDelay 10)
-    ops @?= [WODone ()]
+    ops0 <- runWidget $ liftSafeBlockingIO (threadDelay 10)
+    ops0 @?= [WODone ()]
+    ops1 <- runWidget $ liftUnsafeBlockingIO (threadDelay 10)
+    ops1 @?= [WODone ()]
 
 testDisplay = testCase "display" $ do
     ops <- runWidget (display "foo" :: Widget String Void)
