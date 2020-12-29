1f51b570be13cfabd755b444e010b99a1b4163b4
kamoii <>
2020-12-29 20:29:40 +0900

    Add more test

--- concur-core/test/Spec.hs
+++ concur-core/test/Spec.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE TypeApplications #-}
+
 import Concur.Core
+import Control.Applicative (Alternative ((<|>)))
+import Control.Concurrent (threadDelay)
 import Control.Monad.Free (Free (Free, Pure))
+import Data.Functor (($>))
+import qualified Data.IORef as IORef
 import Data.Void (Void)
 import Test.Tasty
 import Test.Tasty.HUnit (testCase, (@?=))
-import Control.Concurrent (threadDelay)
-import Control.Applicative (Alternative((<|>)))
 
 data WidgetOp v a
     = WOView v
@@ -33,22 +36,65 @@ main =
             , testAlternative
             ]
 
-testAlternative = testCase "Alternative" $ do
-    let io = liftSafeBlockingIO (threadDelay 10)
-    ops0 <- runWidget $ io <|> display "a"
-    ops0 @?= [WOView "a", WODone ()]
+testAlternative :: TestTree
+testAlternative =
+    testGroup
+        "Alternative"
+        [ testCase "immediate" $ do
+            -- left-biased
+            ops <- runWidget $ pure "a" <|> pure "b"
+            ops @?= [WODone "a"]
+        , testCase "immediate'" $ do
+            ops <- runWidget $ display "a" <|> pure ()
+            ops @?= [WODone ()]
+        , testCase "immediate''" $ do
+            ops <- runWidget $ pure () <|> display "a"
+            ops @?= [WODone ()]
+        , testCase "zero-time" $ do
+            ops <- runWidget $ display "a" <|> waitFor 0
+            ops @?= [WOView "a", WODone ()]
+        , testCase "zero-time'" $ do
+            ops <- runWidget $ waitFor 0 <|> display "a"
+            ops @?= [WOView "a", WODone ()]
+        , testCase "concat" $ do
+            ops <- runWidget $ display "a" <|> display "b" <|> waitFor 10
+            ops @?= [WOView "ab", WODone ()]
+        , testCase "race" $ do
+            ops <- runWidget $ (waitFor1 $> (1 :: Int)) <|> (waitFor2 $> 2)
+            ops @?= [WOView "", WODone 1]
+        , testCase "race'" $ do
+            ops <- runWidget $ (waitFor2 $> (1 :: Int)) <|> (waitFor1 $> 2)
+            ops @?= [WOView "", WODone 2]
+        , testCase "cancelling" $ do
+            ref <- IORef.newIORef (1 :: Int)
+            _ps <-
+                runWidget $
+                    waitFor1
+                        <|> ( waitFor2
+                                *> liftSafeBlockingIO (IORef.writeIORef ref 2)
+                            )
+            val0 <- IORef.readIORef ref
+            val0 @?= 1
+            threadDelay 20000
+            val1 <- IORef.readIORef ref
+            val1 @?= 1
+        ]
+  where
+    -- 10000 microsecond is 0.01s.
+    -- これ以上に短かくすると差が小さすぎて,スレッド生成等の時間で埋まってしまい
+    -- 時偶あるべき順序とは逆の順序になってしまう。
+    waitFor1 = waitFor 10000
+    waitFor2 = waitFor 20000
+    waitFor n = liftSafeBlockingIO (threadDelay n)
 
-    ops1 <- runWidget $ do
-        io <|> display "a"
-        display @_ @Void "b"
-    ops1 @?= [WOView "a", WOView "b", WOForever]
-
+testIO :: TestTree
 testIO = testCase "io" $ do
     ops0 <- runWidget $ liftSafeBlockingIO (threadDelay 10)
     ops0 @?= [WODone ()]
     ops1 <- runWidget $ liftUnsafeBlockingIO (threadDelay 10)
     ops1 @?= [WODone ()]
 
+testDisplay :: TestTree
 testDisplay = testCase "display" $ do
     ops <- runWidget (display "foo" :: Widget String Void)
     ops @?= [WOView "foo", WOForever]
