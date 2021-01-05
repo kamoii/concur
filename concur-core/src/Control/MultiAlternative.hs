9e5b68bd76dcd0cfe2d4bc1ccf0aea52e6689c3c
kamoii <>
2021-01-05 23:45:46 +0900

    Format code

--- concur-core/src/Control/MultiAlternative.hs
+++ concur-core/src/Control/MultiAlternative.hs
@@ -1,13 +1,14 @@
-{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE UndecidableInstances #-}
+
 module Control.MultiAlternative where
 
 import Control.Applicative (Alternative, empty, (<|>))
 
 class MultiAlternative f where
-  never :: f a
-  orr :: [f a] -> f a
+    never :: f a
+    orr :: [f a] -> f a
 
 instance {-# OVERLAPPABLE #-} Alternative f => MultiAlternative f where
-  never = empty
-  orr = foldl (<|>) empty
+    never = empty
+    orr = foldl (<|>) empty
