dc5f1e3f73733eea8b9a27575771c8b10d88a73c
kamoii <>
2021-01-05 18:31:51 +0900

    Add MonadView type class and instances

--- concur-core/src/Concur/Core/Types.hs
+++ concur-core/src/Concur/Core/Types.hs
@@ -1,53 +1,58 @@
-{-# LANGUAGE DeriveFunctor              #-}
-{-# LANGUAGE ExistentialQuantification  #-}
-{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE ScopedTypeVariables        #-}
-{-# LANGUAGE StandaloneDeriving         #-}
-{-# LANGUAGE TupleSections              #-}
-module Concur.Core.Types
-  ( Widget(..)
-  , continue
-  , display
-  , mapView
-  , wrapView
-  , SuspendF(..)
-  , awaitViewAction
-  , MultiAlternative(..)
-  , loadWithIO
-  , remoteWidget
-  , unsafeBlockingIO
-  , MonadUnsafeBlockingIO(..)
-  , MonadSafeBlockingIO(..)
-  ) where
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE UndecidableInstances #-}
 
-import           Control.Applicative      (Alternative, empty, (<|>))
-import           Control.Concurrent       (MVar, ThreadId, forkIO, killThread, newEmptyMVar, putMVar, takeMVar)
-import           Control.Monad            (MonadPlus (..), forM)
-import           Control.Monad.Free       (Free (..), hoistFree, liftF)
-import           Control.Monad.IO.Class   (MonadIO, liftIO)
-import           Control.MultiAlternative (MultiAlternative, orr, never)
+module Concur.Core.Types (
+    Widget (..),
+    continue,
+    wrapView,
+    SuspendF (..),
+    awaitViewAction,
+    MultiAlternative (..),
+    loadWithIO,
+    remoteWidget,
+    unsafeBlockingIO,
+    MonadUnsafeBlockingIO (..),
+    MonadSafeBlockingIO (..),
+    MonadView (..),
+) where
 
-import qualified Concur.Core.Notify       as N
+import Control.Applicative (Alternative, empty, (<|>))
+import Control.Concurrent (MVar, ThreadId, forkIO, killThread, newEmptyMVar, putMVar, takeMVar)
+import Control.Monad (MonadPlus (..), forM)
+import Control.Monad.Except (ExceptT, mapExceptT)
+import Control.Monad.Free (Free (..), hoistFree, liftF)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Reader (MonadTrans (lift), ReaderT, mapReaderT)
+import Control.MultiAlternative (MultiAlternative, never, orr)
+
+import qualified Concur.Core.Notify as N
 
 data SuspendF v next
-  = StepView v next
-  | forall r. StepBlock (IO r) (r -> next)
-  | forall r. StepIO    (IO r) (r -> next)
-  | Forever
+    = StepView v next
+    | forall r. StepBlock (IO r) (r -> next)
+    | forall r. StepIO (IO r) (r -> next)
+    | Forever
 
 deriving instance Functor (SuspendF v)
 
-newtype Widget v a = Widget { step :: Free (SuspendF v) a }
-  deriving (Functor, Applicative, Monad)
+newtype Widget v a = Widget {step :: Free (SuspendF v) a}
+    deriving (Functor, Applicative, Monad)
 
-view :: v -> Widget v ()
-view v = Widget $ liftF $ StepView v ()
+_view :: v -> Widget v ()
+_view v = Widget $ liftF $ StepView v ()
 
 effect :: IO a -> Widget v a
 effect a = Widget $ liftF $ StepBlock a id
 
-io :: IO a ->  Widget v a
+io :: IO a -> Widget v a
 io a = Widget $ liftF $ StepIO a id
 
 forever :: Widget v a
@@ -56,55 +61,66 @@ forever = Widget $ liftF Forever
 continue :: SuspendF v a -> Widget v a
 continue = Widget . liftF
 
-display :: v -> Widget v a
-display v = view v >> forever
+_display :: v -> Widget v a
+_display v = _view v >> forever
 
 -- Change the view of a Widget
-mapView :: (u -> v) -> Widget u a -> Widget v a
-mapView f (Widget w) = Widget $ go w
+_mapView :: (u -> v) -> Widget u a -> Widget v a
+_mapView f (Widget w) = Widget $ go w
   where
     go = hoistFree g
-    g (StepView v next)  = StepView (f v) next
-    g (StepIO a next)    = StepIO a next
+    g (StepView v next) = StepView (f v) next
+    g (StepIO a next) = StepIO a next
     g (StepBlock a next) = StepBlock a next
-    g Forever            = Forever
+    g Forever = Forever
 
 -- Generic widget view wrapper
 wrapView :: Applicative f => (u -> v) -> Widget u a -> Widget (f v) a
-wrapView f = mapView (pure . f)
+wrapView f = _mapView (pure . f)
 
--- | IMPORTANT: Blocking IO is dangerous as it can block the entire UI from updating.
---   It should only be used for *very* quick running IO actions like creating MVars.
+{- | IMPORTANT: Blocking IO is dangerous as it can block the entire UI from updating.
+   It should only be used for *very* quick running IO actions like creating MVars.
+-}
 unsafeBlockingIO :: Monoid v => IO a -> Widget v a
 unsafeBlockingIO = io
 
 -- This is a safe use for blockingIO, and is exported
 awaitViewAction :: (N.Notify a -> v) -> Widget v a
 awaitViewAction f = do
-  n <- io N.newNotify
-  view (f n)
-  effect $ N.await n
+    n <- io N.newNotify
+    _view (f n)
+    effect $ N.await n
 
 -- TODO: what to do when the parent dies?
 loadWithIO :: IO a -> Widget v a
 loadWithIO a = do
-  n <- io $ do
-    n <- newEmptyMVar
-    _ <- forkIO $ a >>= putMVar n
-    pure n
-  effect $ takeMVar n
+    n <- io $ do
+        n <- newEmptyMVar
+        _ <- forkIO $ a >>= putMVar n
+        pure n
+    effect $ takeMVar n
 
 -- Make a Widget, which can be pushed to remotely
-remoteWidget :: (MultiAlternative m, MonadUnsafeBlockingIO m, MonadSafeBlockingIO m, MonadIO m, Monad m, Show a) => m b -> (a -> m b) -> IO (a -> m (), m b)
+remoteWidget ::
+    ( MultiAlternative m
+    , MonadUnsafeBlockingIO m
+    , MonadSafeBlockingIO m
+    , MonadIO m
+    , Monad m
+    , Show a
+    ) =>
+    m b ->
+    (a -> m b) ->
+    IO (a -> m (), m b)
 remoteWidget d f = do
-  var <- newEmptyMVar
-  return (proxy var, wid var d)
+    var <- newEmptyMVar
+    return (proxy var, wid var d)
   where
     proxy var = liftUnsafeBlockingIO . putMVar var
     wid var ui = orr [Left <$> ui, Right <$> liftSafeBlockingIO (takeMVar var)] >>= either return (wid var . f)
 
 instance Monoid v => MonadIO (Widget v) where
-  liftIO = loadWithIO
+    liftIO = loadWithIO
 
 -- IMPORTANT NOTE: This Alternative instance is NOT the same one as that for Free.
 -- That one simply uses Alternative for Suspend. But that one isn't sufficient for us.
@@ -114,82 +130,120 @@ instance Monoid v => MonadIO (Widget v) where
 --         Left distributivity (of fmap):  f <$> (a <|> b) = (f <$> a) <|> (f <$> b)
 --  OK     Left absorption (for fmap):  f <$> empty = empty
 instance Monoid v => Alternative (Widget v) where
-  empty = never
-  f <|> g = orr [f,g]
+    empty = never
+    f <|> g = orr [f, g]
 
 stepW :: v -> Free (SuspendF v) a -> IO (Either a (v, Maybe (IO (Free (SuspendF v) a))))
-stepW _ (Free (StepView v next))  = stepW v next
-stepW v (Free (StepIO a next))    = a >>= stepW v . next
+stepW _ (Free (StepView v next)) = stepW v next
+stepW v (Free (StepIO a next)) = a >>= stepW v . next
 stepW v (Free (StepBlock a next)) = pure $ Right (v, Just (a >>= pure . next))
-stepW v (Free Forever)            = pure $ Right (v, Nothing)
-stepW _ (Pure a)                  = pure $ Left a
+stepW v (Free Forever) = pure $ Right (v, Nothing)
+stepW _ (Pure a) = pure $ Left a
 
 instance Monoid v => MultiAlternative (Widget v) where
-  never = display mempty
+    never = _display mempty
 
-  -- Single child fast path without threads
-  orr [w] = go (step w)
-    where
-      go widget = do
-        stepped <- io $ stepW mempty widget
+    -- Single child fast path without threads
+    orr [w] = go (step w)
+      where
+        go widget = do
+            stepped <- io $ stepW mempty widget
 
-        case stepped of
-          Left a                -> pure a
-          Right (v, Nothing)    -> view v >> forever
-          Right (v, Just await) -> do
-            view v
-            next <- effect await
-            go next
+            case stepped of
+                Left a -> pure a
+                Right (v, Nothing) -> _view v >> forever
+                Right (v, Just await) -> do
+                    _view v
+                    next <- effect await
+                    go next
 
-  -- General threaded case
-  orr ws = do
-    mvar <- io newEmptyMVar
-    comb mvar $ fmap (Left . step) ws
-    where
-      comb :: MVar (Int, Free (SuspendF v) a) -> [Either (Free (SuspendF v) a) (v, Maybe ThreadId)] -> Widget v a
-      comb mvar widgets = do
-        stepped <- io $ forM widgets $ \w -> case w of
-          Left suspended         -> either Left (Right . Left) <$> stepW mempty suspended
-          Right (displayed, tid) -> pure $ Right $ Right (displayed, tid)
-  
-        case sequence stepped of
-          -- A widget finished, kill all running threads
-          Left a -> do
-            io $ sequence_
-              [ killThread tid
-              | Right (Right (_, Just tid)) <- stepped
-              ]
-            pure a
-          Right next -> do
-            -- Display all current views
-            view $ mconcat $ map (either fst fst) next
+    -- General threaded case
+    orr ws = do
+        mvar <- io newEmptyMVar
+        comb mvar $ fmap (Left . step) ws
+      where
+        comb :: MVar (Int, Free (SuspendF v) a) -> [Either (Free (SuspendF v) a) (v, Maybe ThreadId)] -> Widget v a
+        comb mvar widgets = do
+            stepped <- io $
+                forM widgets $ \w -> case w of
+                    Left suspended -> either Left (Right . Left) <$> stepW mempty suspended
+                    Right (displayed, tid) -> pure $ Right $ Right (displayed, tid)
 
-            tids <- io $ forM (zip [0..] next) $ \(i, v) -> case v of
-              -- Start a new thread on encountering StepBlock
-              Left (dv, Just await) -> fmap (Right . (dv,) . Just) $ forkIO $ do
-                a <- await
-                putMVar mvar (i, a)
+            case sequence stepped of
+                -- A widget finished, kill all running threads
+                Left a -> do
+                    io $
+                        sequence_
+                            [ killThread tid
+                            | Right (Right (_, Just tid)) <- stepped
+                            ]
+                    pure a
+                Right next -> do
+                    -- Display all current views
+                    _view $ mconcat $ map (either fst fst) next
 
-              -- Neverending Widget, pass on
-              Left  (dv, Nothing)   -> pure $ Right (dv, Nothing)
+                    tids <- io $
+                        forM (zip [0 ..] next) $ \(i, v) -> case v of
+                            -- Start a new thread on encountering StepBlock
+                            Left (dv, Just await) -> fmap (Right . (dv,) . Just) $
+                                forkIO $ do
+                                    a <- await
+                                    putMVar mvar (i, a)
 
-              -- Already running, pass on
-              Right (dv, tid)       -> pure $ Right (dv, tid)
-                  
-            (i, newWidget) <- effect $ takeMVar mvar
-            comb mvar (take i tids ++ [Left newWidget] ++ drop (i + 1) tids)
+                            -- Neverending Widget, pass on
+                            Left (dv, Nothing) -> pure $ Right (dv, Nothing)
+                            -- Already running, pass on
+                            Right (dv, tid) -> pure $ Right (dv, tid)
+
+                    (i, newWidget) <- effect $ takeMVar mvar
+                    comb mvar (take i tids ++ [Left newWidget] ++ drop (i + 1) tids)
 
 -- The default instance derives from Alternative
 instance Monoid v => MonadPlus (Widget v)
 
-class MonadUnsafeBlockingIO m where
-  liftUnsafeBlockingIO :: IO a -> m a
+-- | MonadUnsafeBlockingIO
+class Monad m => MonadUnsafeBlockingIO m where
+    liftUnsafeBlockingIO :: IO a -> m a
 
 instance MonadUnsafeBlockingIO (Widget v) where
-  liftUnsafeBlockingIO = io
+    liftUnsafeBlockingIO = io
 
-class MonadSafeBlockingIO m where
-  liftSafeBlockingIO :: IO a -> m a
+instance MonadUnsafeBlockingIO m => MonadUnsafeBlockingIO (ReaderT r m) where
+    liftUnsafeBlockingIO = lift . liftUnsafeBlockingIO
+
+instance MonadUnsafeBlockingIO m => MonadUnsafeBlockingIO (ExceptT e m) where
+    liftUnsafeBlockingIO = lift . liftUnsafeBlockingIO
+
+-- | MonadSafeBlockingIO
+class Monad m => MonadSafeBlockingIO m where
+    liftSafeBlockingIO :: IO a -> m a
 
 instance MonadSafeBlockingIO (Widget v) where
-  liftSafeBlockingIO = effect
+    liftSafeBlockingIO = effect
+
+instance MonadSafeBlockingIO m => MonadSafeBlockingIO (ReaderT r m) where
+    liftSafeBlockingIO = lift . liftSafeBlockingIO
+
+instance MonadSafeBlockingIO m => MonadSafeBlockingIO (ExceptT e m) where
+    liftSafeBlockingIO = lift . liftSafeBlockingIO
+
+-- | MonadView
+class Monad m => MonadView v m | m -> v where
+    view :: v -> m ()
+    display :: v -> m a
+    mapView :: (v -> v) -> m a -> m a
+
+instance MonadView v (Widget v) where
+    view = _view
+    display = _display
+    mapView = _mapView
+
+instance MonadView v m => MonadView v (ReaderT r m) where
+    view = lift . view
+    display = lift . display
+    mapView f = mapReaderT (mapView f)
+
+instance MonadView v m => MonadView v (ExceptT v m) where
+    view = lift . view
+    display = lift . display
+    mapView f = mapExceptT (mapView f)
